51267941cd52c8dbe38056fac9d453c891602a5f
parinaz <parinaz.mehr1370@gmail.com>
2018-02-09 16:18:58 +0330

    sql sample

--- src/main/java/com/taxikar/repository/JurneyDriverRepository.java
+++ src/main/java/com/taxikar/repository/JurneyDriverRepository.java
@@ -2,9 +2,12 @@ package com.taxikar.repository;
 
 import com.taxikar.entity.JurneyDriver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Created by parinaz on 2/9/2018.
  */
 public interface JurneyDriverRepository extends JpaRepository<JurneyDriver, Long> {
+    @Query("select jr from jurney_driver jr where jr.price=?1")
+    JurneyDriver getSpecificPrice(Long price);
 }
